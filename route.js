58a4584a5d15cad5d3d4271593992b31b8c8d15f
tempostorm <vip.a.d.2014@mail.ru>
2017-06-28 13:48:03 +0400

    Update route.js

--- route.js
+++ route.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const mongoose  = require('mongoose');
 mongoose.Promise = global.Promise;
 const {user_model} = require('./mongo_models');
@@ -14,12 +13,7 @@ mongoose.connection.on('connected', function () {
 mongoose.connection.on('error',function (err) {
     console.log('Mongoose default connection error: ');
 });
-// const mongo =require('mongodb').MongoClient;
-const url='mongodb://localhost:27017/todo';
-=======
-const mongo =require('mongodb').MongoClient;
-const url='mongodb://localhost:27017/todo'
->>>>>>> 901ea6b9dc4f27b356596c099e349cdade654e8b
+
 router.get('/', (req, res) => {
     res.render('node');
 });
@@ -50,28 +44,7 @@ router.post('/register', (req, res) => {
     user_model.create(req.body)
 res.send('Perfect!');
 });
-router.post('/insert',(req,res, next)=>{
-    const ppl={
-        username: req.body.username,
-        phone_number: req.body.phone_number,
-        email: req.body.email,
-        password: req.body.password,
-    };
-    mongo.connect(url, function(err, db) {
-        if(!err) {
-            console.log("We are connected");
-        }
 
-
-        db.collection('user').insertOne(ppl,(err,result)=>{
-            if(!err){
-                console.log('ppl inserted')
-            }
-            db.close()
-        })
-    });
-    res.redirect('/ok')
-})
 
 
 module.exports = router;
